e411c5065b94bb535a74b212b5f429fc73e6ef40
nVar <nkeledjian@gmail.com>
2018-10-25 22:17:05 -0700

    Working on starting/stoping animation of gifs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-    var themes = [];
+    // "Dogs", "Cats", "Birds", "Bears", "Llama"
+    var themes = ["Dogs", "Cats", "Birds", "Bears", "Llama"];
+
     function renderBtns() {
-
+        // prevents rendering of previous theme button
         $("#gif-btns").empty();
-
         // Loop through array of themes
-        for (var i = 0; i < themes.length; i++) {
+        for (var b = 0; b < themes.length; b++) {
             // Then dynamicaly generate buttons for each theme in arr.
             // $("<button>") will create the start and end tag. (<button></button>)
             var b = $("<button>");
             // Adding a class
-            b.addClass("theme");
-            console.log("b tag", b);
+            b.addClass("theme-btn");
             // Adding b data-attribute with b value of the theme at index i
             b.attr("theme-name", themes[i]);
             // Providing the button's text with b value of the theme at index i
             b.text(themes[i]);
-            // Adding the button to the HTML
+            // Add to HTML
             $("#gif-btns").append(b);
         }
     }
@@ -24,7 +24,6 @@ $(document).ready(function () {
         // event.preventDefault() prevents the form from trying to submit itself.
         //form used so user can hit enter instead of clicking the button if they want
         event.preventDefault();
-
         // This line will grab the text from the input box
         var theme = $("#theme-input").val().trim();
         // The theme from the textbox is then added to our array
@@ -33,34 +32,57 @@ $(document).ready(function () {
         // call the render buttons function - 
         renderBtns();
     });
-    // $(document).on(function () { // needed for following .on functions?
-        $(document).on("click", "button", function () {
-            console.log('i am clicking....')
-            console.log('this thing', this)
-            var themeChoice = $(this).attr("theme-name");
-            console.log(themeChoice);
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + themeChoice + "&api_key=iPmT5nea5OlsvqUmyx7SyJfzmxb6FHGy&limit=10";
-            // Call AJAX HTTP get request with above URL convention
-            $.ajax({
-                url: queryURL,
-                method: "GET",
-            }).then(function (response) {
-                // store response.data to var results for convenience
-                var results = response.data;
+    
+    $(document).on("click", "theme-btn", function () {
+        var themeChoice = $(this).attr("theme-name");
+        console.log(themeChoice);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + themeChoice + "&api_key=iPmT5nea5OlsvqUmyx7SyJfzmxb6FHGy&limit=10";
+        // Call AJAX HTTP get request with above URL convention
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            // store response.data to var results for convenience
+            var results = response.data;
 
-                console.log("Results", response.data);
-                for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $("<div>");
-                    
-                    var themeImg = $("<img>");
+            console.log("Results", response.data);
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+                
+                var p = $("<p>").text("Rating: " + results[i].rating);
 
-                    themeImg.attr("src", results[i].images.fixed_height.url);
+                var themeImg = $("<img>");
 
-                    gifDiv.append(themeImg);
+                themeImg.attr("class", "gif");
+        
+                themeImg.attr("data-state", "still");
+                
+                themeImg.attr("data-still", results[i].images.fixed_width_still.url);
 
-                    $("#gifs-here").prepend(gifDiv);
-                }
-            });
+                themeImg.attr("data-animate", results[i].images.fixed_width.url );
+
+                themeImg.attr("src", results[i].images.fixed_height.url);
+
+                themeImage.attr("src", results[i].images.fixed_width_still.url);
+
+                gifDiv.append(themeImg);
+
+                gifDiv.append(p);
+
+                $("#gifs-here").prepend(gifDiv);
+            }
         });
-    // }); // Document.on(function) CLOSING  
+        $(".gif").on("click", function() {
+            console.log("this",this);
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
+    });
+    
 }); // Document.ready(function) CLOSING
